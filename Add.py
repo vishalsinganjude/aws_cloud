0f76dc178c20a9f1e1690a892804236818832b83
vishalsinganjude <vsinganjude8@gmail.com>
2020-05-01 13:29:55 +0530

    Add two numbers in python

---
+++ Add.py
@@ -0,0 +1,14 @@
+num1 = 12
+num2 = 13
+num = num1+num2
+print(num1+num2)
+print(num)
+
+#print floating sum from input from user
+
+a = input("1st num : ")
+b = input("2nd num : ")
+
+sum = float(a)+float(b)
+#printing values
+print("sum of {0} and {1} is {2}".format(a,b,sum))
