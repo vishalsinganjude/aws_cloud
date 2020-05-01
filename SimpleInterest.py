3718f75c4d6416b8dcf3b42692a5cf46a7e2208a
vishalsinganjude <vsinganjude8@gmail.com>
2020-05-01 14:23:14 +0530

    Find out Simple Interest in python

---
+++ SimpleInterest.py
@@ -0,0 +1,17 @@
+
+P = 10000
+R = 5
+T = 5
+
+SI = (P*R*T)/100
+print("The simple interest is", SI)
+
+#2nd Example
+'''
+a=300
+b=7
+c=1
+Si=int(a*b*c)/100
+
+print("The simple interest is", Si)
+'''
